529fd712e8867d8022f1376431fad1dee6028013
Tran Thanh Tung <tranthanhtung@Trans-MBP.net.fpt>
2017-05-12 15:14:11 +0700

    make auth

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// home page route
+Route::get('/', 'PagesController@index');
 
 //Test route
 Route::get('test','TestController@index');
+
+// Auth route
+Auth::routes();
 
